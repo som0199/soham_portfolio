02cf653009752c7a23b37ac847111148dcacd155
Soham Bijghawne <somyagurjar786@gmail.com>
2024-06-05 23:18:05 +0530

    #update

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,14 +4,14 @@ import Button from "react-bootstrap/Button";
 import { AiOutlineDownload } from "react-icons/ai";
 
 import Particle from "../components/Particle";
-import pdf from "../assets/Soham MERN.pdf";
+import pdf from "../assets/Soham-MERN.pdf";
 
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-const resumeLink = `https://raw.githubusercontent.com/som0199/portfolio/main/src/assets/Soham MERN.pdf`;
+const resumeLink = `https://raw.githubusercontent.com/som0199/portfolio/main/src/assets/Soham-MERN.pdf`;
 
 const Resume = () => {
   const [width, setWidth] = useState(1200);
